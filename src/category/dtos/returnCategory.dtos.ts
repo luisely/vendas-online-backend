e29f804bfcd77a6535eb4e78396debbbc1f839ad
Fernando Ely <luisely@gmail.com>
2023-04-24 09:21:44 -0300

    feat: create category Service

---
+++ src/category/dtos/returnCategory.dtos.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { CategoryEntity } from '../entities/category.entity';
+
+export class ReturnCategory {
+  id: number;
+  name: string;
+
+  constructor(categoryEntity: CategoryEntity) {
+    this.id = categoryEntity.id;
+    this.name = categoryEntity.name;
+  }
+}
