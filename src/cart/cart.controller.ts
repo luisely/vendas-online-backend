38a45c938636f29da5bfb649872f7f9d48e2d6c1
Fernando Ely <luisely@gmail.com>
2023-04-25 17:44:00 -0300

    feat: create metod clearCart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Delete } from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
 import { InsertCart } from './dtos/insert-cart-dto';
-import { CartEntity } from './entities/cart.entity';
 import { CartService } from './cart.service';
 import { UserId } from 'src/decorators/user-id.decorator';
 import { ReturnCartDto } from './dtos/return-cart.dto';
+import { DeleteResult } from 'typeorm';
 
 @Roles(UserType.User, UserType.Admin)
 @Controller('cart')
@@ -20,5 +20,17 @@ export class CartController {
     return new ReturnCartDto(
       await this.cartService.insertProductInCart(insertCart, userId),
     );
+  }
+
+  @Get()
+  async findCartByUserId(@UserId() userId: number): Promise<ReturnCartDto> {
+    return new ReturnCartDto(
+      await this.cartService.findCartByUserId(userId, true),
+    );
+  }
+
+  @Delete()
+  async clearCart(@UserId() userId: number): Promise<DeleteResult> {
+    return this.cartService.clearCart(userId);
   }
 }
